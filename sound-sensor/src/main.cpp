e2c075cee00396cbe569a952148c4b14087468ef
Alex Kalineskou <alexkalineskou.dev@gmail.com>
2025-01-03 06:49:36 +0200

    Add more leds to sound-sensor

--- sound-sensor/src/main.cpp
+++ sound-sensor/src/main.cpp
@@ -1,8 +1,11 @@
 #include "Arduino.h"
 
 #define SENSOR_PIN 1
-#define LED_DUM_PIN 41
-#define LED_TEK_PIN 42
+#define LED_DUM_PIN1 41
+#define LED_DUM_PIN2 40
+#define LED_TEK_PIN1 42
+#define LED_TEK_PIN2 39
+#define LED_TEK_PIN3 38
 
 void setup() {
   // fast baud for IR receiver
@@ -15,43 +18,70 @@ void setup() {
 
   pinMode(SENSOR_PIN, INPUT);
 
-  pinMode(LED_DUM_PIN,OUTPUT);
-  pinMode(LED_TEK_PIN,OUTPUT);
+  pinMode(LED_DUM_PIN1,OUTPUT);
+  pinMode(LED_DUM_PIN2,OUTPUT);
+  pinMode(LED_TEK_PIN1,OUTPUT);
+  pinMode(LED_TEK_PIN2,OUTPUT);
+  pinMode(LED_TEK_PIN3,OUTPUT);
 
   Serial.println("setup done");
 
   // dum+
-  digitalWrite(LED_DUM_PIN,HIGH);
-  digitalWrite(LED_TEK_PIN,HIGH);
+  digitalWrite(LED_DUM_PIN1,HIGH);
+  digitalWrite(LED_DUM_PIN2,HIGH);
+  digitalWrite(LED_TEK_PIN1,HIGH);
+  digitalWrite(LED_TEK_PIN2,HIGH);
+  digitalWrite(LED_TEK_PIN3,HIGH);
   delay(50);
-  digitalWrite(LED_DUM_PIN,LOW);
-  digitalWrite(LED_TEK_PIN,LOW);
+  digitalWrite(LED_DUM_PIN1,LOW);
+  digitalWrite(LED_DUM_PIN2,LOW);
+  digitalWrite(LED_TEK_PIN1,LOW);
+  digitalWrite(LED_TEK_PIN2,LOW);
+  digitalWrite(LED_TEK_PIN3,LOW);
   delay(2 * 100);
 
   // tek
-  digitalWrite(LED_TEK_PIN,HIGH);
+  digitalWrite(LED_TEK_PIN1,HIGH);
+  digitalWrite(LED_TEK_PIN2,HIGH);
+  digitalWrite(LED_TEK_PIN3,HIGH);
   delay(50);
-  digitalWrite(LED_TEK_PIN,LOW);
+  digitalWrite(LED_TEK_PIN1,LOW);
+  digitalWrite(LED_TEK_PIN2,LOW);
+  digitalWrite(LED_TEK_PIN3,LOW);
   delay(1 * 100);
 
   // ke
-  digitalWrite(LED_TEK_PIN,HIGH);
+  digitalWrite(LED_TEK_PIN1,HIGH);
+  digitalWrite(LED_TEK_PIN2,HIGH);
+  digitalWrite(LED_TEK_PIN3,HIGH);
   delay(50);
-  digitalWrite(LED_TEK_PIN,LOW);
+  digitalWrite(LED_TEK_PIN1,LOW);
+  digitalWrite(LED_TEK_PIN2,LOW);
+  digitalWrite(LED_TEK_PIN3,LOW);
   delay(1 * 100);
 
   // dum+
-  digitalWrite(LED_DUM_PIN,HIGH);
-  digitalWrite(LED_TEK_PIN,HIGH);
+  digitalWrite(LED_DUM_PIN1,HIGH);
+  digitalWrite(LED_DUM_PIN2,HIGH);
+  digitalWrite(LED_TEK_PIN1,HIGH);
+  digitalWrite(LED_TEK_PIN2,HIGH);
+  digitalWrite(LED_TEK_PIN3,HIGH);
   delay(50);
-  digitalWrite(LED_DUM_PIN,LOW);
-  digitalWrite(LED_TEK_PIN,LOW);
+  digitalWrite(LED_DUM_PIN1,LOW);
+  digitalWrite(LED_DUM_PIN2,LOW);
+  digitalWrite(LED_TEK_PIN1,LOW);
+  digitalWrite(LED_TEK_PIN2,LOW);
+  digitalWrite(LED_TEK_PIN3,LOW);
   delay(2 * 100);
 
   // ke+
-  digitalWrite(LED_TEK_PIN,HIGH);
+  digitalWrite(LED_TEK_PIN1,HIGH);
+  digitalWrite(LED_TEK_PIN2,HIGH);
+  digitalWrite(LED_TEK_PIN3,HIGH);
   delay(50);
-  digitalWrite(LED_TEK_PIN,LOW);
+  digitalWrite(LED_TEK_PIN1,LOW);
+  digitalWrite(LED_TEK_PIN2,LOW);
+  digitalWrite(LED_TEK_PIN3,LOW);
 }
 
 void loop() {
@@ -67,8 +97,11 @@ void loop() {
 
   if (samplesAverage < 25) {
     // treat as not playing below this threshold
-    digitalWrite(LED_DUM_PIN,LOW);
-    digitalWrite(LED_TEK_PIN,LOW);
+    digitalWrite(LED_DUM_PIN1,LOW);
+    digitalWrite(LED_DUM_PIN2,LOW);
+    digitalWrite(LED_TEK_PIN1,LOW);
+    digitalWrite(LED_TEK_PIN2,LOW);
+    digitalWrite(LED_TEK_PIN3,LOW);
 
     return;
   }
@@ -77,7 +110,9 @@ void loop() {
 
   // treat as tek
   Serial.println("tek");
-  digitalWrite(LED_TEK_PIN,HIGH);
+  digitalWrite(LED_TEK_PIN1,HIGH);
+  digitalWrite(LED_TEK_PIN2,HIGH);
+  digitalWrite(LED_TEK_PIN3,HIGH);
 
   if (samplesAverage < 1500) {
     return;
@@ -85,5 +120,6 @@ void loop() {
 
   // treat as dum above this threshold
   Serial.println("dum");
-  digitalWrite(LED_DUM_PIN,HIGH);
+  digitalWrite(LED_DUM_PIN1,HIGH);
+  digitalWrite(LED_DUM_PIN2,HIGH);
 }
