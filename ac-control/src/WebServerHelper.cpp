0f97b4a8ac971aeffe4d20987c10a462aae6e72c
Alex Kalineskou <alexkalineskou.dev@gmail.com>
2025-01-06 00:23:21 +0200

    Add force a/c start and stop + Improve web server html

--- ac-control/src/WebServerHelper.cpp
+++ ac-control/src/WebServerHelper.cpp
@@ -23,30 +23,60 @@ void WebServerHelper::setup() {
 <head>
   <title>A/C Control</title>
   <meta charset="utf-8">
-  <meta http-equiv="refresh" content="10">
+  <meta http-equiv="refresh" content="30">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/3274/3274588.png" />
+  <style>
+    body {
+      text-align: center;
+      margin-top: 8em;
+      background-color: black;
+      color: white;
+    }
+
+    a {
+      text-decoration:none;
+      color: lightskyblue;
+    }
+    a:hover {
+      color: #ffffff;
+    }
+    a:active {
+      color: #78adff;
+    }
+  </style>
 </head>
-<body style="text-align: center;">
-  <p>__ENABLED_TEMPLATE__</p>
-  <p>Temperature in: __TEMPERATURE_IN__.</p>
-  <p>Temperature Out: __TEMPERATURE_OUT__.</p>
-  <p>Last A/C Command: __LAST_AC_COMMAND__.</p>
+<body>
+  <p>
+    __ENABLED_TEMPLATE__
+  </p>
+  <p>
+    Last A/C Command: <b>__LAST_AC_COMMAND__</b>
+  </p>
+  <p>
+    Force A/C Command: <b><a href="/force-ac-start">Start</a></b> | <b><a href="/force-ac-stop">Stop</a></b>
+  </p>
   <br>
 
-  <p>Target temperature: __TEMPERATURE_TARGET__.</p>
-  <p>__TEMPERATURE_START_STOP__</p>
+  <p>
+    <a href="/decrease-target">__TARGET_TEMPERATURE_DECREASE__</a>
+    &nbsp; | &nbsp; Target: <b>__TEMPERATURE_TARGET__</b>
+    &nbsp; | &nbsp; <a href="/increase-target">__TARGET_TEMPERATURE_INCREASE__</a>
+  </p>
   <br>
 
-  <p><a href="/increase-target">Increase target temperature by 0.5°C.</a></p>
-  <p><a href="/decrease-target">Decrease target temperature by 0.5°C.</a></p>
+  <p>
+    Temperature: <b>__TEMPERATURE_IN__</b> <small>(__TEMPERATURE_OUT__)</small>
+  </p>
+  <p>
+    __TEMPERATURE_START_STOP__
+  </p>
 </body>
     )==");
 
     html.replace(
       "__ENABLED_TEMPLATE__",
-      String(buttonEnabled.enabled
-               ? R"==(<a href="/disable">Disable A/C control.</a>)=="
-               : R"==(<a href="/enable">Enable A/C control.</a>)==").c_str()
+      buttonEnabled.enabled ? R"==(<a href="/disable">Disable A/C Control</a>)==" : R"==(<a href="/enable">Enable A/C Control</a>)=="
     );
     html.replace("__TEMPERATURE_IN__", TemperatureSensor::formatTemperature(temperatureSensorManager.temperatureIn()).c_str());
     html.replace("__TEMPERATURE_OUT__", TemperatureSensor::formatTemperature(temperatureSensorManager.temperatureOut()).c_str());
@@ -54,12 +84,26 @@ void WebServerHelper::setup() {
 
     html.replace("__TEMPERATURE_TARGET__", TemperatureSensor::formatTemperature(temperatureData.temperatureTarget).c_str());
     if (acMode == Cold) {
-      html.replace("__TEMPERATURE_START_STOP__", "Stop when < __TEMPERATURE_STOP__ | Start when > __TEMPERATURE_START__");
+      html.replace(
+        "__TEMPERATURE_START_STOP__",
+        "Stop <small>&lt;</small> <b>__TEMPERATURE_STOP__</b> &nbsp; | &nbsp; Start <small>&gt;</small> <b>__TEMPERATURE_START__</b>"
+      );
     } else {
-      html.replace("__TEMPERATURE_START_STOP__", "Start when < __TEMPERATURE_START__ | Stop when > __TEMPERATURE_STOP__");
+      html.replace(
+        "__TEMPERATURE_START_STOP__",
+        "Start <b><small>&lt;</small> __TEMPERATURE_START__</b> &nbsp; | &nbsp; Stop <b><small>&gt;</small> __TEMPERATURE_STOP__</b>"
+      );
     }
     html.replace("__TEMPERATURE_START__", TemperatureSensor::formatTemperature(temperatureData.temperatureTargetStart()).c_str());
     html.replace("__TEMPERATURE_STOP__", TemperatureSensor::formatTemperature(temperatureData.temperatureTargetStop()).c_str());
+    html.replace(
+      "__TARGET_TEMPERATURE_INCREASE__",
+      TemperatureSensor::formatTemperature(temperatureData.temperatureTarget + static_cast<int>(0.5 * 10)).c_str()
+    );
+    html.replace(
+      "__TARGET_TEMPERATURE_DECREASE__",
+      TemperatureSensor::formatTemperature(temperatureData.temperatureTarget - static_cast<int>(0.5 * 10)).c_str()
+    );
 
     webServer.send(200, "text/html", html.c_str());
   });
@@ -89,9 +133,21 @@ void WebServerHelper::setup() {
     webServer.sendHeader("Location", "/", true);
     webServer.send(302);
   });
+  webServer.on("/force-ac-start", HTTP_GET, [this] {
+    infraredTransmitter.sendCommand(Start, true);
+
+    webServer.sendHeader("Location", "/", true);
+    webServer.send(302);
+  });
+  webServer.on("/force-ac-stop", HTTP_GET, [this] {
+    infraredTransmitter.sendCommand(Stop, true);
+
+    webServer.sendHeader("Location", "/", true);
+    webServer.send(302);
+  });
 
   webServer.onNotFound([this] {
-    webServer.send(404, "text/html", String(R"==(
+    webServer.send(404, "text/html", R"==(
 <html>
 <head>
   <title>Resource not found</title>
@@ -102,7 +158,7 @@ void WebServerHelper::setup() {
   <p>The resource was not found.</p>
   <p><a href="/">Start again</a></p>
 </body>
-    )==").c_str());
+    )==");
   });
 }
 
